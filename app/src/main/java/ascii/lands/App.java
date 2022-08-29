d97775c00df3d882683a3980addec6bd8287f211
Mclilzee <mc.lilzee@hotmail.com>
2022-08-29 16:44:32 +0200

    Create talking donkey class

--- app/src/main/java/ascii/lands/App.java
+++ app/src/main/java/ascii/lands/App.java
@@ -6,6 +6,7 @@ package ascii.lands;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Donkey donkey = new Donkey();
+        donkey.say("hello");
     }
 }
